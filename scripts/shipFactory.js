3a072134ab70de862e37f83b0e838dc74ab21bf6
Marshnme <jildsmiles@yahoo.com>
2022-06-28 16:57:46 -0400

    testing shipFactory function and public methods

--- scripts/shipFactory.js
+++ scripts/shipFactory.js
@@ -1,10 +1,25 @@
-const shipFactory = (length, hitLocations = [] , sunk = false) => {
-    length,
-    hitLocation,
-    sunk,
+const shipFactory = (length) => {
+    const hitLocations = [];
+    const sunk = false;
 
-    return { length, hitLocation, sunk };
+    function hit(num) {
+        if (hitLocations.includes(num)) {
+            return 'already hit';
+        }
+        if (num <= length) {
+            this.hitLocations.push(num);
+        } else {
+            return 'You missed';
+        }
+    }
+
+    function destroy() {
+        if (this.hitLocations.length === this.length) {
+            this.sunk = true;
+        }
+    }
+
+    return { length, hitLocations, sunk, hit, destroy };
 };
 
-
-export default shipFactory
+export { shipFactory };
---
+++ scripts/shipTests/shipTests.test.js
@@ -0,0 +1,30 @@
+import { shipFactory } from '../shipFactory';
+
+const newShip = shipFactory(4);
+test('creates ship', () => {
+    expect(newShip.length).toBe(4);
+});
+
+test('ship tracks hits to self', () => {
+    newShip.hit(2);
+    expect(newShip.hitLocations).toContain(2);
+});
+
+test('ship doesnt track hit if larger than length', () => {
+    newShip.hit(27);
+    expect(newShip.hitLocations).not.toContain(27);
+});
+
+test('ship doesnt track same hit twice', () => {
+    newShip.hit(2);
+    expect(newShip.hitLocations).toHaveLength(1);
+});
+
+test('ship is destroyed ', () => {
+    const destroyMe = shipFactory(3);
+    destroyMe.hit(1);
+    destroyMe.hit(2);
+    destroyMe.hit(3);
+    destroyMe.destroy();
+    expect(destroyMe.sunk).toBe(true);
+});
