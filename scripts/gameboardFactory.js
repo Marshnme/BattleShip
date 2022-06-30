f3fbceca9ddababfa02fa1fbe766d430ccb51aaf
Marshnme <jildsmiles@yahoo.com>
2022-06-29 21:09:44 -0400

    cleaned up reused code

--- scripts/gameboardFactory.js
+++ scripts/gameboardFactory.js
@@ -22,21 +22,29 @@ const gameBoardFactory = () => {
     }
     function receiveAttack(cord1) {
         const updatedShips = this.ships.map((ship) => {
-            if (ship.cord1.split('-')[0] === ship.cord2.split('-')[0]) {
-                if (cord1.split('-')[0] === ship.cord1.split('-')[0]) {
-                    for (let i = 1; i <= ship.cord2.split('-')[1]; i++) {
-                        if (cord1 === `${ship.cord1.split('-')[0]}-${[i]}`) {
+            // column ships hit detection
+            const shipCord1FirstValue = ship.cord1.split('-')[0];
+            const shipCord1SecondValue = ship.cord1.split('-')[1];
+            const shipCord2FirstValue = ship.cord2.split('-')[0];
+            const shipCord2SecondValue = ship.cord2.split('-')[1];
+            const attackCordFirstValue = cord1.split('-')[0];
+
+            if (shipCord1FirstValue === shipCord2FirstValue) {
+                if (attackCordFirstValue === shipCord1FirstValue) {
+                    for (let i = 1; i <= shipCord2SecondValue; i++) {
+                        if (cord1 === `${shipCord1FirstValue}-${[i]}`) {
                             ship.ship.hit(i);
                         }
                         if (
-                            cord1.split('-')[1] < ship.cord1.split('-')[1] ||
-                            cord1.split('-')[1] > ship.cord2.split('-')[1]
+                            cord1.split('-')[1] < shipCord1SecondValue ||
+                            cord1.split('-')[1] > shipCord2SecondValue
                         ) {
                             missedShot(cord1);
                         }
                     }
                 }
             }
+            // row ship hit detection
         });
     }
 
