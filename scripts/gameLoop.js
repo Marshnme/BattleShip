c0e396ef2b60c0020b5c74c9db0ba43fd771df23
Marshnme <jildsmiles@yahoo.com>
2022-07-01 16:18:41 -0400

    created game loop module and test for players

---
+++ scripts/gameLoop.js
@@ -0,0 +1,7 @@
+import { gameBoardFactory } from './gameboardFactory';
+import shipFactory from './shipFactory';
+import player from './player-ai';
+
+const gameLoop = () => {};
+
+export default gameLoop;
--- scripts/player-AiTests/player-ai.test.js
+++ scripts/player-AiTests/player-ai.test.js
@@ -1,1 +1,13 @@
 import player from '../player-ai.js';
+import { gameBoardFactory } from '../gameboardFactory.js';
+
+const boardForTester = gameBoardFactory();
+boardForTester.placeShip(1, 'A-1', 'A-1');
+boardForTester.placeShip(4, 'C-4', 'F-4');
+
+const testPlayer = player('tester', boardForTester);
+
+test('player attack is tracked', () => {
+    testPlayer.attack('C-4');
+    expect(testPlayer.allAttacks).toEqual(['C-4']);
+});
